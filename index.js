ff58bcfe022bbbec08ed62d2e70cc01b46a8afc5
Jim Zuccon <zuccon@gmail.com>
2017-10-31 13:51:16 +1100

    Initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+import ElCreate from './src/Create';
+import ElDelete from './src/Delete';
+import ElEdit from './src/Edit';
+import ElCrud from './src/ElCrud';
+import ElList from './src/List';
+import ElShow from './src/Show';
+
+export {
+  ElCreate,
+  ElDelete,
+  ElEdit,
+  ElCrud,
+  ElList,
+  ElShow,
+}
